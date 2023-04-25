930fed9fbe0323554a4c54b66570a5a3cf1df78c
atslice <alice.patel2008@gmail.com>
2023-04-25 16:09:34 +0800

    operator

--- 04_calculation/calculation.go
+++ 04_calculation/calculation.go
@@ -10,16 +10,50 @@ func minus(x, y float64) float64 {
 	return x - y // minus: -
 }
 
+// more than: >
 func greater(x, y float64) bool {
 	return x > y
 }
 
+// less than: <
 func smaller(x, y float64) bool {
 	return x < y
 }
 
+// equal: =
 func equal(x, y float64) bool {
 	return x == y
+}
+
+// non-equal: !=
+func nonequal(x, y float64) bool {
+	return x != y
+}
+
+// more than or equal to: >=
+func greater_equal(x, y float64) bool {
+	return x >= y
+}
+
+// less than or equal to: <=
+func smaller_equal(x, y float64) bool {
+	return x <= y
+}
+
+// logic operation
+// logic and: &&
+func and(x, y bool) bool {
+	return x && y
+}
+
+// logic or: ||
+func or(x, y bool) bool {
+	return x || y
+}
+
+// logic not: !
+func not(x bool) bool {
+	return !x
 }
 
 func main() {
@@ -29,4 +63,14 @@ func main() {
 	fmt.Println("9 is greater than 9.0:", greater(9, 9.0))
 	fmt.Println("9 is smaller than 9.0:", smaller(9, 9.0))
 	fmt.Println("9 is equal to 9.0:", equal(9, 9.0))
+	fmt.Println("9 is not equal to 9.0:", nonequal(9, 9.0))
+	fmt.Println("9 is greater than or equal to 9.0:", greater_equal(9, 9.0))
+	fmt.Println("9 is less than or equal to 9.0:", smaller_equal(9, 9.0))
+	// fmt.Println("0 && 1:", and(0, 1))  // Build Error: cannot use 0 (untyped int constant) as bool value in argument to and; cannot use 1 (untyped int constant) as bool value in argument to and
+	fmt.Println("true && true:", and(5 > 2, 3 > 1))
+	// i := 0
+	fmt.Println("false && true:", and(5 < 2, true))
+	fmt.Println("false || true:", or(5 < 2, true))
+	fmt.Println("true || true:", or(5 >= 2, true))
+	fmt.Println("not true:", not(true))
 }
