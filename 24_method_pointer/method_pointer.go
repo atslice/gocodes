77cc741ef8b4740dcf4317b04e1a856de2a30204
atslice <alice.patel2008@gmail.com>
2023-07-25 11:53:21 +0800

    method

--- 24_method_pointer/method_pointer.go
+++ 24_method_pointer/method_pointer.go
@@ -25,6 +25,8 @@ func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// function, the parameter v is a pointer
+// if called, the passed parameter v must be a pointer but not a value
 func Scale(v *Vertex, f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
@@ -33,11 +35,14 @@ func Scale(v *Vertex, f float64) {
 func main() {
 	v1 := Vertex{3, 4}
 	fmt.Println(v1.Abs())
-	v1.Scale(10)
-	fmt.Println(v1.Abs())
+	v1.Scale(10)          // though v1 is value, and the method Scale appect a pointer as receiver parameter, it is OK
+	fmt.Println(v1.Abs()) // 50
+	(&v1).Scale(10)       // pointer is also OK
+	fmt.Println(v1.Abs()) // 500
 
 	v := Vertex{3, 4}
 	fmt.Println(Abs(v))
+	// Scale(v, 10)  // cannot use v (variable of type Vertex) as *Vertex value in argument to Scale
 	Scale(&v, 10)
 	fmt.Println(Abs(v))
 }
